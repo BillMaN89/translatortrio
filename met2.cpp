9af0c9ec2e9b9b992984124577a083321b79c82c
Alex-Laios <alex.p.laios@gmail.com>
2018-12-19 21:34:05 +0200

    met2 se C++ me string anti lista giati variomoun th zoh mou alla ola ok

--- met2.cpp
+++ met2.cpp
@@ -1,23 +1,56 @@
 #include <iostream>
-#include <list>
 #include <string>
 #include <stdlib.h>
 #include <time.h>
 
 using namespace std;
 
-int main(){
+int main(void){
 	
-	list<string> stringlist = {"ekfrasi"};
+	string str1 = "ekfrasi";
 	int i=0,count=1;
 	srand (time(NULL));
 	
-	while(i<=stringlist.size()){
-		if(stringlist.front()=="ekfrasi"{
-			int x =rand() % 2 + 1;
-			
+	while(i<=str1.length()){
+		
+		if(str1.substr(i,7)=="ekfrasi"){
+			cout<<"Step "<<count<<": "<<str1<<endl;
+			int x =rand() % 10;
+			if ((x%2)==1){
+				str1.replace(i,7,"oros");
+			}
+			else{
+				str1.insert(i+7,"+oros");
+			}
 		}
+		else if(str1.substr(i,4)=="oros"){
+			cout<<"Step "<<count<<": "<<str1<<endl;
+			int x =rand() % 10 ;
+			if ((x%2)==1){
+				str1.replace(i,4,"paragontas");
+			}
+			else{
+				str1.insert(i+4,"*paragontas");
+			}
+		}
+		else if(str1.substr(i,10)=="paragontas"){
+			cout<<"Step "<<count<<": "<<str1<<endl;
+			int x =rand() % 3 + 1;
+			if (x==1){
+				str1.replace(i,10,"a");
+			}
+			else if (x==2){
+				str1.replace(i,10,"b");
+			}
+			else{
+				str1.replace(i,10,"c");
+			}
+		}
+		else{
+			i+=1;
+			count--;
+		}
+		count++;
 	}
-
-	return 0;
+	cout<<"Generated String: "<<str1<<endl;
 }
