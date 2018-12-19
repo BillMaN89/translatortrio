881f5920ed3d17434b523938ff790999cbbbde65
Vasilis Synagridis <39857823+BillMaN89@users.noreply.github.com>
2018-12-19 17:10:00 +0200

    To met2 ce C++ because pikrakis

---
+++ met2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+	
+	list<string> stringlist = {"ekfrasi"};
+	int i=0,count=1;
+	srand (time(NULL));
+	
+	while(i<=stringlist.size()){
+		if(stringlist.front()=="ekfrasi"{
+			int x =rand() % 2 + 1;
+			
+		}
+	}
+
+	return 0;
+}
