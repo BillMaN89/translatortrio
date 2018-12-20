775c352959e0a468afa8e2e80aeff94fd5f2617e
Alex-Laios <alex.p.laios@gmail.com>
2018-12-20 19:48:38 +0200

    To kana na pairnei string kai einai upertata gamhstero

--- met3.cpp
+++ met3.cpp
@@ -1,84 +1,81 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
 
-int main() {
-  char input[] = {'[', '[', 'y', ':', 'x', ']', '+', '[', 'x', ':', 'y', ']', ']','$'};
-  string array[4][7];
-  array[0][0]="S->[A]";
-  array[1][0]="A->BE";
-  array[1][4]="A->BE";
-  array[1][5]="A->BE";
-  array[2][0]="B->S";
-  array[2][4]="B->x";
-  array[2][5]="B->y";
-  array[3][1]="E->e";
-  array[3][2]="E->:A";
-  array[3][3]="E->+A";
-  stack<char> topdownsyntax;
-  topdownsyntax.push('$');
-  topdownsyntax.push('S');
-  int i = 0;
-  while (i<14) {
-    if (topdownsyntax.top() == input[i]) {
-      topdownsyntax.pop();
-      i++;
-    }
-    else if (topdownsyntax.top()=='S'){
-      cout <<array[0][0]<<endl;
-      topdownsyntax.pop();
-      topdownsyntax.push(']');
-      topdownsyntax.push('A');
-      topdownsyntax.push('[');
-    }
-    else if (topdownsyntax.top()=='A') {
-      cout <<array[1][4]<<endl;
-      topdownsyntax.pop();
-      topdownsyntax.push('E');
-      topdownsyntax.push('B');
-    }
-    else if (topdownsyntax.top()=='B') {
-      if (input[i]=='x') {
-        cout <<array[2][4]<<endl;
-        topdownsyntax.pop();
-        topdownsyntax.push('x');
-      }
-      else if (input[i]=='y') {
-        cout <<array[2][5]<<endl;
-        topdownsyntax.pop();
-        topdownsyntax.push('y');
-      }
-      else if (input[i]=='[') {
-        cout <<array[2][0]<<endl;
-        topdownsyntax.pop();
-        topdownsyntax.push('S');
-      }
-    }
-    else if (topdownsyntax.top()=='E') {
-      if (input[i]==':') {
-        cout <<array[3][2]<<endl;
-        topdownsyntax.pop();
-        topdownsyntax.push('A');
-        topdownsyntax.push(':');
-      }
-      else if (input[i]=='+') {
-        cout <<array[3][3]<<endl;
-        topdownsyntax.pop();
-        topdownsyntax.push('A');
-        topdownsyntax.push('+');
-      }
-      else{
-        cout <<array[3][1]<<endl;
-        topdownsyntax.pop();
-      }
-    }
-  }
- cout<<endl<<"The result is: "<<endl;
-  if (topdownsyntax.empty()) {
-    cout<<"Yes"<<endl;
-  }
-  else{
-    cout<<"No"<<endl;
-  }
-  return 0;
+int main(void) {
+
+	string input;
+	cout<<"Do you want to insert your own string for check, or use the default one([[y:x]+[x:y]])?"<<endl;
+	cout<<"Insert your string now with '$' at the end or type 'no' to use the default"<<endl;
+	cin>>input;
+	if (input=="no")
+		input = "[[y:x]+[x:y]]$";
+  	stack<char> topdownsyntax;
+  	topdownsyntax.push('$');
+  	topdownsyntax.push('S');
+	  int i = 0;
+	  while (i<input.length()) {
+	    if (topdownsyntax.top() == input[i] && input[i]!='$' && topdownsyntax.top()!='$') {
+	      topdownsyntax.pop();
+	      i++;
+	    }
+	    else if (input[i]=='$' && topdownsyntax.top()=='$')
+	    {
+	    	cout<<endl<<"The result is:"<<endl<<"Yes"<<endl;
+	    	break;
+	    }
+	    else if (topdownsyntax.top()=='S'){
+	      cout <<"S->[A]"<<endl;
+	      topdownsyntax.pop();
+	      topdownsyntax.push(']');
+	      topdownsyntax.push('A');
+	      topdownsyntax.push('[');
+	    }
+	    else if (topdownsyntax.top()=='A') {
+	      cout <<"A->BE"<<endl;
+	      topdownsyntax.pop();
+	      topdownsyntax.push('E');
+	      topdownsyntax.push('B');
+	    }
+	    else if (topdownsyntax.top()=='B') {
+	      if (input[i]=='x') {
+	        cout <<"B->x"<<endl;
+	        topdownsyntax.pop();
+	        topdownsyntax.push('x');
+	      }
+	      else if (input[i]=='y') {
+	        cout <<"B->y"<<endl;
+	        topdownsyntax.pop();
+	        topdownsyntax.push('y');
+	      }
+	      else if (input[i]=='[') {
+	        cout <<"B->S"<<endl;
+	        topdownsyntax.pop();
+	        topdownsyntax.push('S');
+	      }
+	    }
+	    else if (topdownsyntax.top()=='E') {
+	      if (input[i]==':') {
+	        cout <<"E->:A"<<endl;
+	        topdownsyntax.pop();
+	        topdownsyntax.push('A');
+	        topdownsyntax.push(':');
+	      }
+	      else if (input[i]=='+') {
+	        cout <<"E->+A"<<endl;
+	        topdownsyntax.pop();
+	        topdownsyntax.push('A');
+	        topdownsyntax.push('+');
+	      }
+	      else{
+	        cout <<"E->e"<<endl;
+	        topdownsyntax.pop();
+	      }
+	    }
+	    else{
+	      	cout<<endl<<"The result is:"<<endl<<"No"<<endl;
+			break;
+	    }
+	  }
 }
