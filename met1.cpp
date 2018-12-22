70deba6ccc1a098156c016b22517bcada4ceec4f
Alex-Laios <alex.p.laios@gmail.com>
2018-12-22 19:01:46 +0200

    Met1 me to cout pou elege o stef

--- met1.cpp
+++ met1.cpp
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[]) {
 	stack<char> nas;
 	char x;
 	int steps;
-
+	nas.push('$');
 	ifstream myfile;
 	myfile.open(argv[1]);
 	cout << "Do you want to display the steps done?" << endl<<"Type 1 for yes,2 for no"<<endl;
@@ -21,32 +21,29 @@ int main(int argc, char const *argv[]) {
 				cout << "Left Parenthesis Found. Stack status: (k1, A inserted)"<<endl;
 		}
 		else if (x == ')') {
-			if (!nas.empty()) {
+			if (nas.top()!='$') {
 				nas.pop();
 	       		if (steps==1)
 					cout << "Right Parenthesis Found. Stack status: (k1, A popped out)"<<endl;
 			}
-		else {
-        	nas.push('A');
-       		if (steps==1)
-				cout << "Rigt Parenthesis Found without a Left to close. Stack status: (k2, A inserted)"<<endl;
-				break;
+			else {
+        		nas.push('A');
+       			if (steps==1){
+					cout << "Rigt Parenthesis Found without a Left to close. No rule found for that situation."<<endl;
+					break;
+				}
 			}
 		}
 	}
-	if (nas.empty()) {
-		if (steps==1)
-			cout<<"No more Parenthesis to insert. Stack status: (k2,$)"<<endl;
-		cout << "YES"<<endl;
-	}
-	else {
-		if (steps==1)
-			cout<<"No more Parenthesis to insert. Stack status: (k2,A)"<<endl;
-		cout << "NO"<<endl;
-	}
-
-
-
-
+		if (nas.top()=='$') {
+			if (steps==1)
+				cout<<"No more Parenthesis to insert. Stack status: (k2,$)"<<endl;
+			cout << "YES"<<endl;
+		}
+		else {
+			if (steps==1)
+				cout<<"No more Parenthesis to insert. Stack status: (k2,A)"<<endl;
+			cout << "NO"<<endl;
+		}
 	return 0;
 }
